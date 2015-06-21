8ff9a04440000a040316d100d88f5ec9d0440d3c
Anil N <anilknyn@yahoo.com>
2015-06-21 19:53:13 +0530

    Use ring signal

--- ht9032_read/ht9032_read.ino
+++ ht9032_read/ht9032_read.ino
@@ -1,21 +1,31 @@
 // -*- mode: c++ -*-
-
 #include <Arduino.h>
-#include "printf.h"
 
-#define DIN 2
-#define DEB 8
+int serial_putc( char c, FILE * )
+{
+  Serial.write( c );
+  return c;
+}
+
+void printf_begin(void)
+{
+  fdevopen( &serial_putc, 0 );
+}
+
+#define RING 3
+#define DIN  2
+#define DEB  8
 
 void dump_hex(const char *msg, unsigned char *buf, int len) {
-     int i;
-     printf_P(PSTR("%s"), msg);
-     for (i = 0; i<len; i++) {
-	  if ((i % 16) == 0) {
-	       printf_P(PSTR("\n%03x: "), i);
-	  }
-	  printf_P(PSTR(" %02x"), buf[i]);
-     }
-     printf_P(PSTR("\n"));
+  int i;
+  printf_P(PSTR("%s"), msg);
+  for (i = 0; i<len; i++) {
+    if ((i % 16) == 0) {
+      printf_P(PSTR("\n%03x: "), i);
+    }
+    printf_P(PSTR(" %02x"), buf[i]);
+  }
+  printf_P(PSTR("\n"));
 }
 
 void setup ()
@@ -24,6 +34,7 @@ void setup ()
   printf_begin();
 
   pinMode(DIN, INPUT);
+  pinMode(RING, INPUT);
   pinMode(DEB, OUTPUT);
 }
 
@@ -39,7 +50,8 @@ struct t_cnd_msg {
   char number[16];
 
   byte cksum;
-} cnd_msg;
+}
+cnd_msg;
 
 bool parse_cnd_msg(byte *buf, byte len, struct t_cnd_msg *msg) {
   byte *bp = buf;
@@ -92,7 +104,8 @@ Error:
 void doSeizure() {
   byte v, oldv = 0x4;
   byte bits = 0;
-  byte driftlen = 0, maxdrift = 0;;
+  byte driftlen = 0, maxdrift = 0;
+  ;
 
   printf_P(PSTR("Waiting.\r\n"));
 
@@ -106,11 +119,12 @@ void doSeizure() {
     v = PIND & 0x4;
     bits += ((v ^ oldv)>>2);
     oldv = v;
-  } while (bits < 10);
+  }
+  while (bits < 10);
 
   PORTB &= 0xFE;
 
-  delay(360);
+  delay(150);
 
   PORTB |= 1;
 }
@@ -118,67 +132,116 @@ void doSeizure() {
 struct t_cnd_msg msg;
 byte buf[32];
 
-void loop ()
-{
+void getClip () {
   byte *bp = buf;
   byte len = 0;
   int stc;
-  memset(buf, 0, 32);
 
-  doSeizure();
+  memset(buf, 0, 32);
   while (PIND & 4);
 
   do {
-     byte v = 0, aux = 0;
+    byte v = 0, aux = 0;
 
-     /* Start bit */
-     PORTB &= 0xFE;
-     delayMicroseconds(414);
-     delayMicroseconds(415);
+    /* Start bit */
+    PORTB &= 0xFE;
+    delayMicroseconds(414);
+    delayMicroseconds(415);
 
-     for (byte i=8; i>0; i--) {
-       PORTB &= 0xFE;
-       delayMicroseconds(415);
+    for (byte i=8; i>0; i--) {
+      PORTB &= 0xFE;
+      delayMicroseconds(415);
 
-       PORTB |= 1;
-       aux = (PIND & 0x4);
-       delayMicroseconds(250);
-       aux |= (PIND & 0x4);
-       v = (v >> 1) | (aux << 5);
-       delayMicroseconds(161);
-     }
+      PORTB |= 1;
+      aux = (PIND & 0x4);
+      delayMicroseconds(250);
+      aux |= (PIND & 0x4);
+      v = (v >> 1) | (aux << 5);
+      delayMicroseconds(161);
+    }
 
-     *bp = v;
-     bp ++;
-     len ++;
+    *bp = v;
+    bp ++;
+    len ++;
 
-     /* Stop bit */
-     PORTB &= 0xFE;
-     delayMicroseconds(414);
-     delayMicroseconds(415);
+    /* Stop bit */
+    PORTB &= 0xFE;
+    delayMicroseconds(414);
+    delayMicroseconds(415);
 
-     stc = 0;
-     while ((PIND & 4) && (stc < 1200)) stc++;
-     if (stc > 1200) {
-       printf("stc = %d\r\n", stc);
-     }
-   } while (stc < 1200);
-   *bp = 0;
+    stc = 0;
+    while ((PIND & 4) && (stc < 1200)) stc++;
+    if (stc > 1200) {
+      printf("stc = %d\r\n", stc);
+    }
+  }
+  while (stc < 1200);
+  *bp = 0;
 
-   delay(200);
-   while (!(PIND & 0x4));
+  delay(200);
+  while (!(PIND & 0x4));
 
-//   if (len < 0x10) {
- //    len |= 0x10;
+  //   if (len < 0x10) {
+  //    len |= 0x10;
   // }
 
-   printf("Len = %d\r\n", len);
-   dump_hex("Stuff", buf, len);
-   if (parse_cnd_msg(buf, len, &msg)) {
-     printf_P(PSTR("Timestamp = %02d - %02d : %02d:%02d hours\r\n"), msg.month, msg.day, msg.hour, msg.minute);
-     printf_P(PSTR("Calling line number = %s\r\n"), msg.number);
-     printf_P(PSTR("Tag = %02x, Checksum = %02x, Len = %02x\r\n"), msg.tag, msg.cksum, msg.len);
-   } else {
-     printf_P(PSTR("Parse failed (Len = %d)\r\n"), len);
-   }
+  printf("Len = %d\r\n", len);
+  dump_hex("Stuff", buf, len);
+  if (parse_cnd_msg(buf, len, &msg)) {
+    printf_P(PSTR("Timestamp = %02d - %02d : %02d:%02d hours\r\n"), msg.month, msg.day, msg.hour, msg.minute);
+    printf_P(PSTR("Calling line number = %s\r\n"), msg.number);
+    printf_P(PSTR("Tag = %02x, Checksum = %02x, Len = %02x\r\n"), msg.tag, msg.cksum, msg.len);
+  }
+  else {
+    printf_P(PSTR("Parse failed (Len = %d)\r\n"), len);
+  }
 }
+
+long last_call_first_ring = 0;
+
+void waitForFirstRing() {
+  byte isFirst = false;
+
+  while (!isFirst) {
+    PORTB &= 0xFE;
+    while (!(PIND & _BV(PD3)));
+    PORTB |= 1;
+    printf_P(PSTR("Ring Up\r\n"));
+
+    while (PIND & _BV(PD3));
+    PORTB &= 0xFE;
+    printf_P(PSTR("Ring Down\r\n"));
+
+    delay(300);
+
+    PORTB |= 1;
+    Serial.println(PIND & _BV(PD3));
+    if (PIND & _BV(PD3)) {
+      while (PIND & _BV(PD3));
+      printf_P(PSTR("Is not first\r\n"));
+      isFirst = false;
+      PORTB &= 0xFE;
+      delay(1);
+    }
+    else {
+      if ((millis() - last_call_first_ring) > 3000) {
+        printf_P(PSTR("First Ring\r\n"));
+        last_call_first_ring = millis();
+        PORTB &= 0xFE;
+        isFirst = true;
+      }
+      else {
+        printf_P(PSTR("Second Ring\r\n"));
+        PORTB &= 0xFE;
+      }
+    }
+  }
+}
+
+void loop ()
+{
+  waitForFirstRing();
+  //     doSeizure();
+  getClip();
+}
+
